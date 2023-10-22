d30e4cdfacb25becbc2c2160bfe48d115806fcd4
NBojan <63514753+NBojan@users.noreply.github.com>
2023-10-22 06:13:44 -0500

    seo, logo

--- app/utils/helpingHand.js
+++ app/utils/helpingHand.js
@@ -1,5 +1,6 @@
-import logo from "@/public/NBChattenzi.png";
 import { MdPeopleOutline, MdGamepad, MdSportsSoccer  } from "react-icons/md";
+
+const logo = "https://firebasestorage.googleapis.com/v0/b/nb-chatroom.appspot.com/o/NBChattenzi.png?alt=media&token=3b5b3dbe-c0f0-4228-bc26-e0e4d0ce93db&_gl=1*gol4sv*_ga*MTU3NTE4NzY2Ny4xNjkzOTEzMDI2*_ga_CW55HF8NVT*MTY5Nzk3MTEwNy4zNC4xLjE2OTc5NzMxMDUuNjAuMC4w";
 
 const rooms = [
     {
@@ -29,7 +30,7 @@ const myMetadata = {
     title: "NB-Chattenzi",
     description:
       "NB Chattenzi chatroom. Multiple rooms to explore, talking with anyone, about anyting, simply by visiting the page, no need for registration.",
-    url: "https://nb-instagram.vercel.app/",
+    url: "https://nb-chatroom.vercel.app/",
     siteName: "NB-Chattenzi",
     images: [
       {
